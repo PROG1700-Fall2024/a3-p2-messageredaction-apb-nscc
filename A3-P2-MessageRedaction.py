040afe23475f23c8b906fd7ea32493596778ef00
apb-nscc <W0487099@campus.nscc.ca>
2024-11-13 16:33:43 -0400

    Pseudocode

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -2,11 +2,19 @@
 #Description:   Design and write a program that counts and removes all desired letters from 
 #               any user-entered sentence or phrase.
 
-#Student #:     
-#Student Name:  
+#Student #:     W0487099
+#Student Name:  Alex Barr
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
+
+    #Input Message
+    print("")
+    #Get Letters to Remove
+    #Show How Many Letters Redacted
+    #Print Redactedn Phrase
+    print("\n")
+    #Repeat Input Message
 
 
 
