47c6d216ce11f847adcaa3e098266b31fbd7b4a7
apb-nscc <W0487099@campus.nscc.ca>
2024-11-14 10:18:20 -0400

    Input Proofed

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -11,8 +11,28 @@ def TypePhrase():
 def LetterRedaction(_redactedLetters):
     _letters = input("Type a comma-seperated list of letter to redact: ")
 
+    #Check for spaces at end to remove before the main portion of this function (trying to emulate a C#-like for loop)
+    #I'm doing this because normally a special value will give an invalid input, but a space may be a normal reaction to put
+    #at the end of an input so I don't want to punish the user for that.
+
+    _tempString = [] #Putting the string into an actual list so I can delete the spaces and then will put that list BACK into the original string
+    for letter in _letters:
+        _tempString.append(letter)
+
+    _placeInString = len(_tempString) - 1
+    while _placeInString > -1:
+        if _tempString[_placeInString] == " ":
+            del _tempString[_placeInString]
+            _placeInString -= 1
+        else:
+            break
+    #Put the temporary string back together
+    _letters = ""
+    for i in range(len(_tempString)):
+        _letters += _tempString[i] # THIS IS REALLY COOL THAT THIS WORKS!!!
+
     for i in range(len(_letters)): # Go through the string as a list
-        if i % 2 == 1: # Check every second vslue to make sure that it is a comma (may have to adjust for potential space at the end)
+        if i % 2 == 1: # Check every second vslue in the string -- if it is NOT a comma, end the loop and try again
             if _letters[i] != ",": # if its not a comma tell the user to start over
                 print("Invalid Input. Please try again")
                 LetterRedaction(_redactedLetters)
@@ -21,7 +41,10 @@ def LetterRedaction(_redactedLetters):
                 continue
         else:
             if _letters[i].isalpha():
-                _redactedLetters.append(_letters[i].lower())
+                if _redactedLetters.count(_letters[i]) == 0:
+                    _redactedLetters.append(_letters[i].lower())
+                else:
+                    print("WARNING: Duplicate letters have not been added.")
             else:
                 print("Invalid Input. Please try again")
                 LetterRedaction(_redactedLetters)
@@ -55,11 +78,12 @@ def PrintRedactedPhrase(_phrase, _redactedLetters, _replacementSymbol):
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
-    condition = ""
+    #condition = ""
     endPhrase = "quit"
-    redactedLetters = []
+    redactedLetters = [] #Clear the list so it doesn't cause errors on the next try
 
     while True:
+        redactedLetters = []
         #Input Message
         phrase = TypePhrase()
         if phrase == endPhrase:
